82bfbb3f720880084d703a4804f924e4e7f93619
andi242 <andi242@gmail.com>
2023-10-06 10:34:58 +0200

    add skip import and media scaling

--- alerts-importer/alerts-importer.c
+++ alerts-importer/alerts-importer.c
@@ -1,30 +1,39 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
 
 public class CPHInline
 {
     public bool Execute()
     {
         CPH.LogInfo("import starting");
+        string scale = args["scale"].ToString();
         string alertScene = args["alertScene"].ToString();
+        CPH.ObsSendRaw("CreateScene", "{\"sceneName\":\""+alertScene+"\"}", 0);
         var mp3Dir = args["soundsDir"].ToString();
         var mp4Dir = args["videosDir"].ToString();
+        // positions of scaled video files on canvas (upper left corner of video)
+        var positionX = 100;
+        var positionY = 600;
+        // set supported media file extensions
         List<string> videoFormats = new List<string> { "*.mp4", "*.mov", "*.avi", "*.mkv" };
         List<string> audioFormats = new List<string> { "*.mp3", "*.wav", "*.aac", "*.ogg" };
+        // is obs connected to SB?
         var isConnected = args["obs.isConnected"].ToString();
         if (isConnected != "True" )
         {
             CPH.LogInfo("error: OBS not connected");
             return false;
         }
+        // create list of media files
         List<string> mediaFiles = new List<string>();
-        // video file formats
+        // append video files
         foreach (var format in videoFormats)
         {
             mediaFiles.AddRange(Directory.GetFiles(mp4Dir, format, SearchOption.AllDirectories));
         }
-        // sound file formats
+        // append audio files
         foreach (var format in audioFormats)
         {
             mediaFiles.AddRange(Directory.GetFiles(mp3Dir, format, SearchOption.AllDirectories));
@@ -33,18 +42,33 @@ public class CPHInline
         try
         {
             CPH.LogInfo( mediaFiles.Count + " files total");
+            // if no files to process, exit.
             if (mediaFiles.Count == 0 )
             {
                 CPH.LogInfo("error: " + mediaFiles.Count + " files found");
                 return false;
             }
+            // process file list
             foreach (var file in mediaFiles)
             {
                 string alert = Path.GetFileNameWithoutExtension(file);
                 string jsonPath = file.Replace('\\', '/');
-                CPH.ObsSendRaw("CreateInput", "{\"sceneItemEnabled\": false, \"inputName\": \""+alert+"\",\"inputKind\": \"ffmpeg_source\", \"sceneName\": \""+alertScene+"\",\"inputSettings\": {\"local_file\": \""+jsonPath+"\"}}");
-                CPH.ObsSendRaw("SetInputAudioMonitorType", "{\"inputName\": \""+alert+"\", \"monitorType\": \"OBS_MONITORING_TYPE_MONITOR_AND_OUTPUT\"}");
-                CPH.LogInfo("verarbeitet: " + alert.ToString());
+                string state = CPH.ObsSendRaw("GetInputSettings", "{\"inputName\":\""+alert+"\"}", 0);
+                // if input already exists, skip it.
+                if (state.ToString() == "{}"){
+                    CPH.ObsSendRaw("CreateInput", "{\"sceneItemEnabled\": false, \"inputName\": \""+alert+"\",\"inputKind\": \"ffmpeg_source\", \"sceneName\": \""+alertScene+"\",\"inputSettings\": {\"local_file\": \""+jsonPath+"\"}}");
+                    CPH.ObsSendRaw("SetInputAudioMonitorType", "{\"inputName\": \""+alert+"\", \"monitorType\": \"OBS_MONITORING_TYPE_MONITOR_AND_OUTPUT\"}");
+                    string idJson = CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+alertScene+"\",\"sourceName\":\""+alert+"\",\"searchOffset\":0}", 0);
+                    JToken id = JToken.Parse(idJson);
+                    // the id is only returned if it is a resizable object in OBS. otherwise this would result in a crash the websocket (audio sources do not display, so no resizing on those)
+                    if (id.ToString() != "{}"){
+                        // resize item
+                        CPH.ObsSendRaw("SetSceneItemTransform", "{\"sceneName\":\""+alertScene+"\",\"sceneItemId\":"+id["sceneItemId"]+",\"sceneItemTransform\":{\"scaleX\":"+scale+",\"scaleY\":"+scale+",\"positionX\":"+positionX+",\"positionY\":"+positionY+"}}", 0);
+                    }
+                    CPH.LogInfo("created: " + alert.ToString());
+                } else {
+                    CPH.LogInfo("skipping " + alert.ToString() + ", already present.");
+                }
             }
         }
         catch (Exception ex)
--- alerts-importer/readme.md
+++ alerts-importer/readme.md
@@ -1,11 +1,25 @@
 # Alerts Importer
 
-Import media files from a directory into a scene in OBS.
+Import media files from a directory into a scene in OBS.  
+You can play media files from SB natively, but only audio. And you can't create SB actions from SB itself (afaik).
 
 SB action options:
-- `alertScene`: OBS scene media sources get imported to
+- `alertScene`: OBS scene media sources get imported to, will be created if not exists
 - `soundsDir`: local directory to import sound files from (supported formats: `"*.mp3", "*.wav", "*.aac", "*.ogg"`)
 - `videosDir`: local directory to import video files from (supported formats: `"*.mp4", "*.mov", "*.avi", "*.mkv"`)
+- `scale`: scale video files to given value (e.g.: `0.4` = `40%` in size, `1` is `100%`)
 
 Sources will be named after the file name that is being imported (without file extension).  
-Sources in OBS need to be unique, so if you already have a source with a specific name, this will be overwritten.
+Sources in OBS need to be unique, so if you already have a source with a specific name, this will be skipped.
+
+There is no reliable way to determine the actual size of a video file with OBS websocket in order to decide if scaling is appropriate or not.  
+So every video file is scaled and positioned to setting, no matter what.  
+Adjust sources to your liking after import.
+
+## changelogdiarything
+
+- create OBS scene if not exists
+- added skip import if already exists so existing items will not be reset
+- added scaling of video files and position on creation in OBS scene
+- add video media import
+- init with audio import
