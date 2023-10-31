b08864fcf21cd9ee08a8931d22b3f371009831ce
andi242 <andi242@gmail.com>
2023-10-31 13:31:01 +0100

    add generic timer

---
+++ generic-timer/readme.md
@@ -0,0 +1,20 @@
+# timer
+
+Setup:
+- `timer-setup` action: Optionen anpassen
+- `!timersave` in den chat um Optionen und Einstellungen zu speichern 
+
+Commands:
+- `!timersave` optionen speichern, ggf. in OBS Sourcen anlegen
+- `!timer TEXT,##` starte timer mit ## Minuten und TEXT als Titel. Komma getrennt!
+- `!timerstop` Timer abbrechen
+
+SB action options:
+- `obsScene`: OBS scene für die Text Source
+- `obsText`: OBS Text element name
+- `timerEnde`: OBS Text der nach Ablauf gezeigt wird
+- `finishSound`: Pfad zu einem mp3, das nach Ablauf eines Timers abgespielt wird. Optional, wenn nicht gesetzt wird eine mp3 in das Streamerbot Verzeichnis geladen.
+
+## changelogdiarything
+
+- init
---
+++ generic-timer/timer.c
@@ -0,0 +1,90 @@
+using System;
+using System.Timers;
+using System.Net;
+
+class CPHInline
+{
+    public System.Timers.Timer timerTimer;
+    public int timerSeconds;
+    public string statusText;
+    public string errorMessage = "Error, use command like: !timer pause,10";
+    public string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+    public void Init(){
+        timerTimer = new System.Timers.Timer(1000);
+        timerTimer.Elapsed += OnTimedEvent;
+        timerTimer.AutoReset = true;
+        timerTimer.Enabled = true;
+        timerTimer.Stop();
+    }
+    // setup obs sources    
+    public bool obsSetup(){
+        // set some global vars from arguments
+        try {
+            CPH.SetGlobalVar("timerSound", args["finishSound"].ToString(), true);
+        }
+        catch(Exception ex) {
+            CPH.LogInfo("option not found, downloading sound.");
+            using (var client = new WebClient()) {
+                // download soundalert file to streamerbot directory
+                client.DownloadFile("https://www.myinstants.com/media/sounds/sheep-baaa.mp3", baseDirectory + "timerAlarm.mp3");
+            }
+            CPH.SetGlobalVar("timerSound", baseDirectory + "timerAlarm.mp3", true);
+        }
+        CPH.SetGlobalVar("timerObsScene", args["obsScene"].ToString(), true);
+        CPH.SetGlobalVar("timerObsText", args["obsText"].ToString(), true);
+        CPH.SetGlobalVar("timerEndeText", args["timerEnde"].ToString(), true);
+        // create scene, skipped if exists
+        CPH.ObsSendRaw("CreateScene", "{\"sceneName\":\""+args["obsScene"].ToString()+"\"}", 0);
+        // create text source in obs, skipped if exists
+        CPH.ObsSendRaw("CreateInput", "{\"sceneName\":\""+args["obsScene"].ToString()+"\",\"inputName\":\""+args["obsText"].ToString()+"\",\"inputKind\":\"text_gdiplus_v2\",\"inputSettings\":{\"align\": \"center\",\"valign\": \"center\",\"text\":\"TIMER\", \"bk_color\": 4286513237, \"bk_opacity\": 50, \"color\": 4278190080, \"font\": { \"size\": 512,\"face\": \"Calibri\", \"style\": \"Regular\" }},\"sceneItemEnabled\":true}", 0);
+        CPH.LogInfo("settings saved.");
+        return true;
+    }
+
+    public void OnTimedEvent(Object source, ElapsedEventArgs e){
+        timerSeconds--;
+        TimeSpan time = TimeSpan.FromSeconds(timerSeconds);
+        string cmd = args["command"].ToString().Replace("!", string.Empty);
+        string rawInput = args["rawInput"].ToString();
+        string[] options = rawInput.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        var statusText = options[0];
+        string countdownString = string.Format("{2} | {0}:{1}", (int)time.TotalMinutes, time.ToString("ss"), statusText);
+        if (timerSeconds == 0){
+            stopTimer(CPH.GetGlobalVar<string>("timerEndeText", true));
+            CPH.PlaySound(CPH.GetGlobalVar<string>("timerSound", true), 20, true);
+        } else {
+            CPH.ObsSetGdiText(CPH.GetGlobalVar<string>("timerObsScene", true), CPH.GetGlobalVar<string>("timerObsText", true), countdownString);
+        }
+    }
+
+    public bool startTimer(){
+        string rawInput = args["rawInput"].ToString();
+        string[] options = rawInput.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        if (options.Length != 2){
+            CPH.SendMessage(errorMessage, true);
+            return false;
+        }
+        try {
+            double timerMinutes = Convert.ToDouble(options[1]);
+            CPH.SetGlobalVar("timerDefaultMinutes", timerMinutes, true);
+            timerSeconds = Convert.ToInt32(Math.Floor((timerMinutes * 60) + 1));
+        }
+        catch(Exception ex) {
+            CPH.SendMessage(errorMessage, true);
+            return false;
+        }
+        timerTimer.Start();
+        return true;
+    }
+
+    public bool stopTimer(string text){
+        CPH.ObsSetGdiText(CPH.GetGlobalVar<string>("timerObsScene", true), CPH.GetGlobalVar<string>("timerObsText", true), text);
+        timerTimer.Stop();
+        return true;
+    }
+
+    public bool cancelTimer(){
+        stopTimer(CPH.GetGlobalVar<string>("timerEndeText", true));
+        return true;
+    }
+}
