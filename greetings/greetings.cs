4f8a5e0e2f7bf3febce99a2ecb0b8f393057fdcd
andi242 <andi242@gmail.com>
2023-12-24 20:19:24 +0100

    well, it's all c#, man

--- greetings/greetings.cs
+++ greetings/greetings.cs
@@ -3,21 +3,17 @@ using System;
 class CPHInline
 {
     public void sendGreetings(string userName, string msgId){
-        //string userName = args["userName"].ToString();
         string greetings = CPH.GetTwitchUserVar<string>(userName, "greetings", true);
         if( greetings != null){
             CPH.TwitchReplyToMessage($"{greetings}", msgId, true);
         }
     }
     public void removeGreetings(string userName, string msgId){
-        //string userName = args["userName"].ToString();
         CPH.UnsetTwitchUserVar(userName, "greetings", true);
         CPH.TwitchReplyToMessage($"Begrüßung entfernt.", msgId, true);
     }
 
     public void setGreetings(string userName, string msg, string msgId){
-        //string userName = args["userName"].ToString();
-        // string msg = args["rawInput"].ToString();
         CPH.SetTwitchUserVar(userName, "greetings", msg, true);
         CPH.TwitchReplyToMessage($"deine neue Begrüßung: {msg}", msgId, true);
     }
@@ -28,15 +24,15 @@ class CPHInline
         string msg = args["rawInput"].ToString();
         string msgId = args["msgId"].ToString();
         string command = "";
-        if ( trigger == "First Words"){
+        if (trigger == "First Words"){
             sendGreetings(userName, msgId);
         } else {
             command = args["command"].ToString();
         }
-        if ( command == "!greet set"){
+        if (command == "!greet set"){
             setGreetings(userName, msg, msgId);
         }
-        if ( command == "!greet del"){
+        if (command == "!greet del"){
             removeGreetings(userName, msgId);
         }
         return true;
