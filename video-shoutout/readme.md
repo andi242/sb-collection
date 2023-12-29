9f499d9a9574bbd25f3896c930beeac73566b2ce
andi242 <andi242@gmail.com>
2023-12-29 21:46:44 +0100

    make vso importable with setup

--- video-shoutout/readme.md
+++ video-shoutout/readme.md
@@ -1,14 +1,25 @@
 # video shoutout
 
-# works on my machine
+## features
 
-not intended for importing, just a reference on what to do.
+- `!vsosetup`: creates a new scene with all items for the video shoutout. can be triggered manually right click -> test trigger.
+- `!vso <streamername>`: starts a video shoutout for `streamername`
+- `brb player`: on activation of a scene the player can loop clips from a list of streamers. Increase `loops` for x times the clips, if the list is a bit shorter.
 
-- clip is played in a nested scene (for scaling, texts, effects, etc.)
+## install
+
+- import to streamerbot
+- check settings in `triggerAction`
+  - `triggerScene`: this is your brb scene to host the clip player
+  - `streamers`: comma separated list of streamers
+  - `loops`: loop the list of `streamers` x times
+- check the new scene and customise design, best done when a clip is playing
+- add the new scene to your previously existing scenes as a nested scene
+
+## more
+
+- no html file required
 - queue clip play from a loop into blocking action queue on custom trigger
 - queue gets cleared on scene change, if scene is not whitelisted
-- currently playing clip is being shut down on scene play (no more clips/sounds running in background)
+- currently playing clip is being shut down on scene change (no more clips/sounds running in background)
 - maintains a list of already played clips to prevent more of just the same clips
-- can be triggered from `!vso <user>` command
-
-relies heavily on my local setup, please grab what you need to build your own.
--- video-shoutout/singleVSO-nested.cs
+++ video-shoutout/singleVSO-nested.cs
@@ -6,27 +6,83 @@ using Newtonsoft.Json.Linq;
 
 public class CPHInline
 {
-	public bool Execute()
+    public bool setupVso(){
+        // get vars
+        string soScene = args["Scene"].ToString();
+        string vsource = args["videoSrc"].ToString();
+        string tsource = args["titleSrc"].ToString();
+        string usource = args["userSrc"].ToString();
+        string gsource = args["imageSrc"].ToString();
+        // save settings 
+        CPH.SetGlobalVar("vsoScene", soScene, true);
+        CPH.SetGlobalVar("vsoVideoSrc", vsource, true);
+        CPH.SetGlobalVar("vsoTitleSrc", tsource, true);
+        CPH.SetGlobalVar("vsoUserSrc", usource, true);
+        CPH.SetGlobalVar("vsoImageSrc", gsource, true);
+        CPH.LogInfo($"checking for {soScene}");
+        try {
+            string testId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ vsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+            CPH.LogError($"setup found in {soScene}, will not overwrite.");
+            return false;
+        }
+        catch (System.Exception) {
+            CPH.LogInfo($"{soScene} check failed, proceeding setup.");
+        }
+
+        // create scene
+        CPH.LogInfo($"creating: {soScene}");
+        CPH.ObsSendRaw("CreateScene", "{\"sceneName\":\"" + soScene + "\"}", 0);
+        // create sources in scene
+        CPH.LogInfo($"creating: {vsource}");
+        CPH.ObsSendRaw("CreateInput", "{\"sceneName\":\"" + soScene + "\",\"inputName\":\"" + vsource + "\",\"inputKind\":\"ffmpeg_source\",\"inputSettings\":{\"restart_on_activate\": false, \"close_when_inactive\": true,\"is_local_file\": false},\"sceneItemEnabled\":false}", 0);
+        CPH.ObsSendRaw("SetInputAudioMonitorType", "{\"inputName\": \"" + vsource + "\", \"monitorType\": \"OBS_MONITORING_TYPE_MONITOR_AND_OUTPUT\"}");
+
+        CPH.LogInfo($"creating: {gsource}");
+        CPH.ObsSendRaw("CreateInput", "{\"sceneName\":\"" + soScene + "\",\"inputName\":\"" + gsource + "\",\"inputKind\":\"image_source\",\"inputSettings\":null,\"sceneItemEnabled\":true}", 0);
+        CPH.LogInfo($"creating: {tsource}");
+        CPH.ObsSendRaw("CreateInput", "{\"sceneName\":\"" + soScene + "\",\"inputName\":\"" + tsource + "\",\"inputKind\":\"text_gdiplus_v2\",\"inputSettings\":null,\"sceneItemEnabled\":true}", 0);
+        CPH.LogInfo($"creating: {usource}");
+        CPH.ObsSendRaw("CreateInput", "{\"sceneName\":\"" + soScene + "\",\"inputName\":\"" + usource + "\",\"inputKind\":\"text_gdiplus_v2\",\"inputSettings\":null,\"sceneItemEnabled\":true}", 0);
+
+        // get source IDs
+        string vSourceId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ vsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+        string nTextId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ tsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+        string nUserId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ usource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+        string nGameImgId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ gsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+
+        // format sources
+        CPH.LogInfo($"formatting sources");
+        CPH.ObsSendRaw("SetSceneItemTransform", "{\"sceneName\":\"" + soScene + "\",\"sceneItemId\":"+vSourceId+",\"sceneItemTransform\":{\"boundsHeight\": 1080,\"boundsType\": \"OBS_BOUNDS_SCALE_OUTER\",\"boundsWidth\": 1920,\"alignment\": 5}}", 0);
+        CPH.ObsSendRaw("SetSceneItemTransform", "{\"sceneName\":\"" + soScene + "\",\"sceneItemId\":"+nUserId+",\"sceneItemTransform\":{\"boundsAlignment\": 1,\"boundsHeight\": 264,\"boundsType\": \"OBS_BOUNDS_MAX_ONLY\",\"boundsWidth\":  960,\"alignment\": 5,\"positionX\": 0,\"positionY\": 0}}", 0);
+        CPH.ObsSendRaw("SetSceneItemTransform", "{\"sceneName\":\"" + soScene + "\",\"sceneItemId\":"+nTextId+",\"sceneItemTransform\":{\"boundsAlignment\": 1,\"boundsHeight\": 264,\"boundsType\": \"OBS_BOUNDS_MAX_ONLY\",\"boundsWidth\": 1920,\"alignment\": 5,\"positionX\": 0,\"positionY\": 264}}", 0);
+        CPH.ObsSendRaw("SetSceneItemTransform", "{\"sceneName\":\"" + soScene + "\",\"sceneItemId\":"+nGameImgId+",\"sceneItemTransform\":{\"positionX\": 1620,\"positionY\": 680}}", 0);
+        // decrease vso volume
+        CPH.ObsSendRaw("SetInputVolume", "{\"inputName\":\""+vsource+"\",\"inputVolumeDb\":-20}", 0);
+
+        CPH.ShowToastNotification("", "Streamerbot VSO Setup", $"Please setup your sources in {soScene} to your style and needs.", "","");
+        return true;
+    }
+    
+    public bool Execute()
 	{
-        string soScene = "[n] videoshoutout";
-        string vsource = "vso";
-        string tsource = "clipTitle";
-        string usource = "user";
-        string gsource = "gameImg";
-        // nested scene items
+        string soScene = CPH.GetGlobalVar<string>("vsoScene", true);
+        string vsource = CPH.GetGlobalVar<string>("vsoVideoSrc", true);
+        string tsource = CPH.GetGlobalVar<string>("vsoTitleSrc", true);
+        string usource = CPH.GetGlobalVar<string>("vsoUserSrc", true);
+        string gsource = CPH.GetGlobalVar<string>("vsoImageSrc", true);
+        // get nested scene item ids
         string nSourceId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ vsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
         string nTextId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ tsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
         string nUserId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ usource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
         string nGameImgId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ gsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
-        CPH.LogInfo($"nested scene ids: nSourceId {nSourceId}, nTextId {nTextId}, nUserId {nUserId}, nGameImgId {nGameImgId}");
         List<string> soSources = new List<string> { nGameImgId , nSourceId, nTextId, nUserId };
         
         DateTime now = DateTime.Now;
 		DateTime startdate = DateTime.Now;
 		Random randomNumber = new Random();
 		startdate = startdate.AddDays(-300);
-        string vsoFile = args["vsoFile"].ToString();
         string userName = args["rawInput"].ToString();
+
         List<string> clipList = CPH.GetGlobalVar<List<string>>("clipList", true);
         if (clipList == null){
             clipList = new List<string>();
@@ -44,23 +100,24 @@ public class CPHInline
                 return false;
             }
             string videoUrl = Regex.Replace(clip.ThumbnailUrl, "-preview-.*",".mp4");
-            string videoplayerfile = vsoFile;
-            videoplayerfile += "?video=" + videoUrl;
-            int delay = 700 + (int)(clip.Duration * 1000);
-            CPH.ObsSetBrowserSource(soScene, vsource, videoplayerfile);
+
+            int delay = 1000 + (int)(clip.Duration * 1000);
+            CPH.ObsSetMediaSourceFile(soScene, vsource, videoUrl, 0);
             CPH.ObsSetGdiText(soScene, usource, clip.BroadcasterName, 0);
             CPH.ObsSetGdiText(soScene, tsource, clip.Title, 0);
             string gameArt = "https://static-cdn.jtvnw.net/ttv-boxart/" + clip.GameId + "-300x400.jpg";
             CPH.ObsSetImageSourceFile(soScene, gsource, gameArt, 0);
 
             // boot clip
-            CPH.Wait(300);
+            //CPH.Wait(200);
             // send obs raw to preserve transitions
             CPH.LogInfo($"enabling vso sources.");
+            CPH.ObsSendRaw("SetInputMute", "{\"inputName\":\""+vsource+"\",\"inputMuted\":false}", 0);
             foreach (var source in soSources) {
                 CPH.ObsSendRaw("SetSceneItemEnabled", "{\"sceneName\":\""+ soScene +"\",\"sceneItemId\":"+ source +",\"sceneItemEnabled\":true}", 0);
             }
-            CPH.Wait(800); //for animations
+            CPH.Wait(1000); //for animations
+            CPH.LogInfo($"playing clip: {clip.Url}");
             
             // uncomment to post link to clip link to chat
             //CPH.SendMessage($"Clip von @{clip.BroadcasterName}: {clip.Url}", true);
@@ -70,6 +127,7 @@ public class CPHInline
 
             // shutdown clip
             CPH.LogInfo($"disabling vso sources.");
+            CPH.ObsSendRaw("SetInputMute", "{\"inputName\":\""+vsource+"\",\"inputMuted\":true}", 0);
             foreach (var source in soSources) {
                 CPH.ObsSendRaw("SetSceneItemEnabled", "{\"sceneName\":\""+ soScene +"\",\"sceneItemId\":"+ source +",\"sceneItemEnabled\":false}", 0);
             }
@@ -79,7 +137,7 @@ public class CPHInline
 
             CPH.Wait(1000); //for animations
             // reset sources
-            CPH.ObsSetBrowserSource(soScene, vsource, "about:blank");
+            CPH.ObsSendRaw("SetInputSettings", "{\"inputName\":\""+vsource+"\",\"inputSettings\":{\"input\":\"\"},\"overlay\":true}", 0);
             CPH.ObsSetGdiText(soScene, usource, "", 0);
             CPH.ObsSetGdiText(soScene, tsource, "", 0);
             CPH.ObsSetImageSourceFile(soScene, gsource, "", 0);
@@ -89,4 +147,54 @@ public class CPHInline
         }
 		return true;
 	}
+    public bool triggerLoop(){
+        int count = int.Parse(args["loops"].ToString());
+        string scenes = args["triggerScene"].ToString();
+        string queue = "vso"; // the blocking queue, this should be imported with the sb imports
+        string soScene = CPH.GetGlobalVar<string>("vsoScene", true);
+        string vsource = CPH.GetGlobalVar<string>("vsoVideoSrc", true);
+        string tsource = CPH.GetGlobalVar<string>("vsoTitleSrc", true);
+        string usource = CPH.GetGlobalVar<string>("vsoUserSrc", true);
+        string gsource = CPH.GetGlobalVar<string>("vsoImageSrc", true);
+        string nSourceId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ vsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+        string nTextId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ tsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+        string nUserId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ usource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+        string nGameImgId = JToken.Parse(CPH.ObsSendRaw("GetSceneItemId", "{\"sceneName\":\""+ soScene +"\",\"sourceName\":\""+ gsource +"\",\"searchOffset\":0}", 0)).Value<JToken>("sceneItemId").ToString();
+        List<string> soSources = new List<string> { nGameImgId , nSourceId, nTextId, nUserId };
+        // reset the global cliplist var
+        CPH.UnsetGlobalVar("clipList", true);
+        
+        if (scenes.Contains(CPH.ObsGetCurrentScene(0)) == false) {
+            CPH.LogInfo($"{CPH.ObsGetCurrentScene(0)} is not my scene.");
+            CPH.ObsSendRaw("SetInputMute", "{\"inputName\":\""+vsource+"\",\"inputMuted\":true}", 0);
+            foreach (var source in soSources) {
+                CPH.ObsSendRaw("SetSceneItemEnabled", "{\"sceneName\":\""+ soScene +"\",\"sceneItemId\":"+ source +",\"sceneItemEnabled\":false}", 0);   
+            CPH.LogInfo($"sceneItemId {source} disabled.");
+            }
+            CPH.ResumeActionQueue(queue, true);
+            CPH.LogInfo($"{queue} queue cleared.");
+            return false;
+        }
+
+        //string[] streamerlist = streamers.Split(',');
+
+        string streamers = args["streamers"].ToString();
+        string[] names = streamers.Split(',');
+        List<string> streamerlist = new List<string>(names.Length);
+        streamerlist.AddRange(names);
+
+        for (int i = 0; i < count; i++) {
+            var rnd = new Random();
+            var randomList = streamerlist.OrderBy(item => rnd.Next());
+            CPH.LogInfo($"trigger {randomList.Count()} shoutouts.");
+            foreach (var item in randomList) {
+                string name = item.Trim();
+                CPH.SetArgument("rawInput", name);
+                CPH.LogInfo($"added {name} to queue.");
+                CPH.TriggerEvent("soTrigger", true);
+                CPH.Wait(200);
+            }
+        }
+		return true;
+    }
 }
